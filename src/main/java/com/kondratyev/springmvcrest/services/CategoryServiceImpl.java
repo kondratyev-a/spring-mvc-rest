4775c38ed59d2547a9caea3e3f7882ae3978ccde
Кондратьев Алексей Александрович <AKondratyev3@alfabank.ru>
2022-01-03 11:52:27 +0300

    Category controller

--- src/main/java/com/kondratyev/springmvcrest/services/CategoryServiceImpl.java
+++ src/main/java/com/kondratyev/springmvcrest/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.kondratyev.springmvcrest.services;
 import com.kondratyev.springmvcrest.api.v1.mapper.CategoryMapper;
 import com.kondratyev.springmvcrest.api.v1.model.CategoryDTO;
 import com.kondratyev.springmvcrest.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
